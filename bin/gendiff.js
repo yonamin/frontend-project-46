f8ff782c489c7f3d9b2b98ce758d67c6e45ad615
ana <anastasiapak32@gmail.com>
2022-11-21 12:15:36 +0300

    modified gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ const program = new Command();
 program
   .name('gendiff')
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
-
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 program.parse();
