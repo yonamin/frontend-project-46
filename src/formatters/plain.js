d69dc3196e3d98181d4635ac8725a518b749b279
ana <anastasiapak32@gmail.com>
2023-05-20 10:33:00 +0300

    fixed formatters

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -10,35 +10,32 @@ const normalizeValue = (item) => {
   return item;
 };
 
-const plain = (tree) => {
-  const iter = (item, ancestry) => {
-    const getLines = (arr) => arr
-      .filter((node) => node.stage !== 'unchanged')
-      .flatMap((node) => {
-        const { stage } = node;
-        const { name } = node;
-        const newAncestry = `${ancestry}.${name}`;
-        if (stage === 'nested') {
-          return iter(node.children, newAncestry);
-        }
-        const value = normalizeValue(node.value);
-        switch (stage) {
-          case 'added':
-            return `Property '${newAncestry.slice(1)}' was added with value: ${value}`;
-          case 'removed':
-            return `Property '${newAncestry.slice(1)}' was removed`;
-          case 'updated': {
-            const removed = `${normalizeValue(node.value.removed)}`;
-            const added = `${normalizeValue(node.value.added)}`;
-            return `Property '${newAncestry.slice(1)}' was updated. From ${removed} to ${added}`;
-          }
-          default:
-            return `Unknown stage '${stage}'`;
-        }
-      });
-    const lines = getLines(item);
-    return lines.join('\n');
-  };
-  return iter(tree, '');
+const getLines = (arr, ancestry, iter) => arr
+  .filter((node) => node.stage !== 'unchanged')
+  .flatMap((node) => {
+    const { stage, name } = node;
+    const newAncestry = `${ancestry}.${name}`;
+    switch (stage) {
+      case 'added':
+        return `Property '${newAncestry.slice(1)}' was added with value: ${normalizeValue(node.value)}`;
+      case 'removed':
+        return `Property '${newAncestry.slice(1)}' was removed`;
+      case 'updated': {
+        const removed = `${normalizeValue(node.value.removed)}`;
+        const added = `${normalizeValue(node.value.added)}`;
+        return `Property '${newAncestry.slice(1)}' was updated. From ${removed} to ${added}`;
+      }
+      case 'nested': {
+        return iter(node.children, newAncestry);
+      }
+      default:
+        return `Unknown stage '${stage}'`;
+    }
+  });
+const iter = (item, ancestry) => {
+  const lines = getLines(item, ancestry, iter);
+  return lines.join('\n');
 };
+
+const plain = (tree) => iter(tree, '');
 export default plain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,50 +1,47 @@
 import _ from 'lodash';
 
-const stylish = (tree, replacer = ' ', spacesCount = 4) => {
-  const stringify = (item, depth) => {
-    if (!_.isObject(item) || item === null) {
-      return String(item);
+const getLines = (arr, indent, depth, stringify) => arr.flatMap((node) => {
+  const { stage } = node;
+  if (!stage) {
+    const [key, value] = node;
+    return `${indent}  ${key}: ${stringify(value, depth + 1)}`;
+  }
+  const key = node.name;
+  const value = stringify(node.value, depth + 1);
+  switch (stage) {
+    case 'added':
+      return `${indent}+ ${key}: ${value}`;
+    case 'removed':
+      return `${indent}- ${key}: ${value}`;
+    case 'nested':
+      return `${indent}  ${key}: ${stringify(node.children, depth + 1)}`;
+    case 'updated': {
+      const removed = stringify(node.value.removed, depth + 1);
+      const added = stringify(node.value.added, depth + 1);
+      return `${indent}- ${node.name}: ${removed}\n${indent}+ ${node.name}: ${added}`;
     }
+    case 'unchanged':
+      return `${indent}  ${key}: ${value}`;
+    default:
+      return `Unknown stage ${stage}`;
+  }
+});
+const stringify = (item, depth, replacer = ' ', spacesCount = 4) => {
+  if (!_.isObject(item) || item === null) {
+    return String(item);
+  }
 
-    const indentSize = ((depth * spacesCount) - 2);
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - 2);
+  const indentSize = ((depth * spacesCount) - 2);
+  const currentIndent = replacer.repeat(indentSize);
+  const bracketIndent = replacer.repeat(indentSize - 2);
 
-    const getLines = (arr) => arr.flatMap((node) => {
-      const { stage } = node;
-      if (!stage) {
-        const [key, value] = node;
-        return `${currentIndent}  ${key}: ${stringify(value, depth + 1)}`;
-      }
-      const key = node.name;
-      const value = stringify(node.value, depth + 1);
-      switch (stage) {
-        case 'added':
-          return `${currentIndent}+ ${key}: ${value}`;
-        case 'removed':
-          return `${currentIndent}- ${key}: ${value}`;
-        case 'nested':
-          return `${currentIndent}  ${key}: ${stringify(node.children, depth + 1)}`;
-        case 'updated': {
-          const removed = stringify(node.value.removed, depth + 1);
-          const added = stringify(node.value.added, depth + 1);
-          return `${currentIndent}- ${node.name}: ${removed}\n${currentIndent}+ ${node.name}: ${added}`;
-        }
-        case 'unchanged':
-          return `${currentIndent}  ${key}: ${value}`;
-        default:
-          return `Unknown stage ${stage}`;
-      }
-    });
-    const currentVal = _.isPlainObject(item) ? Object.entries(item) : item;
-    const lines = getLines(currentVal);
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-
-  return stringify(tree, 1);
+  const currentVal = _.isPlainObject(item) ? Object.entries(item) : item;
+  const lines = getLines(currentVal, currentIndent, depth, stringify);
+  return [
+    '{',
+    ...lines,
+    `${bracketIndent}}`,
+  ].join('\n');
 };
+const stylish = (tree) => stringify(tree, 1);
 export default stylish;
